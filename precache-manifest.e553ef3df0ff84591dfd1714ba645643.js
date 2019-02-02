2e9c1857816d769e6baae61b95f347add555b10e
Matthew Paterno <mpaterno@live.com>
2019-02-02 09:37:29 -0800

    Updates

---
+++ precache-manifest.e553ef3df0ff84591dfd1714ba645643.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f95bcde29f8fc9426a6fb20311f9a9e",
+    "url": "/static/media/india-grill-9.6f95bcde.png"
+  },
+  {
+    "revision": "ca617984a974fce8dfb63440e570eb2f",
+    "url": "/static/media/india-grill-6.ca617984.png"
+  },
+  {
+    "revision": "0c2a094614b2e42fc5c62077603f407b",
+    "url": "/static/media/india-grill-3.0c2a0946.png"
+  },
+  {
+    "revision": "c50f5f030c36d0aa2fa09e61a6ccc535",
+    "url": "/static/media/india-grill-2.c50f5f03.png"
+  },
+  {
+    "revision": "febe45baecbdc8e3d7f33949370698c3",
+    "url": "/static/media/india-grill-1.febe45ba.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a750736410b903dc3ae6",
+    "url": "/static/js/main.a7507364.chunk.js"
+  },
+  {
+    "revision": "473c858897058fa0c6fe",
+    "url": "/static/js/1.473c8588.chunk.js"
+  },
+  {
+    "revision": "a750736410b903dc3ae6",
+    "url": "/static/css/main.246a187e.chunk.css"
+  },
+  {
+    "revision": "1eadcb620ccac826d91c7b54b3abcd8b",
+    "url": "/index.html"
+  }
+];
